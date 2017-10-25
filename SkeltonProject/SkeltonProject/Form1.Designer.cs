2fb09806f8cf631ee90f3d666651afbdf5b39888
naga1472 <nagata_shinji@skygroup.jp>
2017-10-25 14:52:39 +0900

    スケルトンプロジェクト

---
+++ SkeltonProject/SkeltonProject/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace SkeltonProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("ノード1");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("ノード2");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("ノード3");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("ノード4");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("親ツリー1", new System.Windows.Forms.TreeNode[] {
+            treeNode17,
+            treeNode18,
+            treeNode19,
+            treeNode20});
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("ノード6");
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("ノード7");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("親ツリー2", new System.Windows.Forms.TreeNode[] {
+            treeNode22,
+            treeNode23});
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.Testbutton2 = new System.Windows.Forms.Button();
+            this.Testbutton1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(558, 397);
+            this.tabControl1.TabIndex = 2;
+            this.tabControl1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.treeView1);
+            this.tabPage1.Controls.Add(this.Testbutton2);
+            this.tabPage1.Controls.Add(this.Testbutton1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(550, 371);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("MS UI Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.treeView1.Location = new System.Drawing.Point(6, 154);
+            this.treeView1.Name = "treeView1";
+            treeNode17.Name = "node1";
+            treeNode17.Text = "ノード1";
+            treeNode18.Name = "node2";
+            treeNode18.Text = "ノード2";
+            treeNode19.Name = "node3";
+            treeNode19.Text = "ノード3";
+            treeNode20.Name = "node4";
+            treeNode20.Text = "ノード4";
+            treeNode21.Name = "Pnode1";
+            treeNode21.Text = "親ツリー1";
+            treeNode22.Name = "node6";
+            treeNode22.Text = "ノード6";
+            treeNode23.Name = "node6";
+            treeNode23.Text = "ノード7";
+            treeNode24.Name = "Pnode2";
+            treeNode24.Text = "親ツリー2";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode21,
+            treeNode24});
+            this.treeView1.Size = new System.Drawing.Size(223, 204);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Testbutton2
+            // 
+            this.Testbutton2.Font = new System.Drawing.Font("MS UI Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Testbutton2.Location = new System.Drawing.Point(9, 78);
+            this.Testbutton2.Name = "Testbutton2";
+            this.Testbutton2.Size = new System.Drawing.Size(199, 63);
+            this.Testbutton2.TabIndex = 1;
+            this.Testbutton2.Text = "ボタン2";
+            this.Testbutton2.UseVisualStyleBackColor = true;
+            this.Testbutton2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Testbutton1
+            // 
+            this.Testbutton1.Font = new System.Drawing.Font("MS UI Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Testbutton1.Location = new System.Drawing.Point(9, 14);
+            this.Testbutton1.Name = "Testbutton1";
+            this.Testbutton1.Size = new System.Drawing.Size(200, 58);
+            this.Testbutton1.TabIndex = 0;
+            this.Testbutton1.Text = "ボタン1";
+            this.Testbutton1.UseVisualStyleBackColor = true;
+            this.Testbutton1.Click += new System.EventHandler(this.Testbutton1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.checkedListBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(550, 371);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("MS UI Gothic", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "チェックボックス１",
+            "チェックボックス２",
+            "チェックボックス３",
+            "チェックボックス４",
+            "チェックボックス５",
+            "チェックボックス６",
+            "チェックボックス７",
+            "チェックボックス８",
+            "チェックボックス９"});
+            this.checkedListBox1.Location = new System.Drawing.Point(8, 6);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(532, 344);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.listBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(550, 371);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E"});
+            this.listBox1.Location = new System.Drawing.Point(6, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 220);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(550, 371);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 68);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "タブ2へ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 64);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "タブ3へ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 74);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "タブ4へ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 390);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button Testbutton2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Testbutton1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SkeltonProject/SkeltonProject/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkeltonProject
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ボタン2が押されました");
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("リストが押されました");
+        }
+
+        private void Testbutton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ボタン1が押されました");
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(1);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(3);
+        }
+    }
+}
