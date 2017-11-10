369385118cf8ed61b3376ef49b5b55d1d6272399
naga1472 <nagata_shinji@skygroup.jp>
2017-11-10 14:30:51 +0900

    コミット

--- SkeltonProject/SkeltonProject/Form1.cs
+++ SkeltonProject/SkeltonProject/Form1.cs
@@ -44,7 +44,7 @@ namespace SkeltonProject
 
         private void Testbutton1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ボタttanンーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー");
+            MessageBox.Show("ボタttanンーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー");
         }
 
         
