1feae8a3dd2509d7f3766dce079992925e61ebe2
naga1472 <nagata_shinji@skygroup.jp>
2017-11-13 10:26:56 +0900

    ボタン1の文言を変更

--- SkeltonProject/SkeltonProject/Form1.cs
+++ SkeltonProject/SkeltonProject/Form1.cs
@@ -44,7 +44,7 @@ namespace SkeltonProject
 
         private void Testbutton1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ボタン1");
+            MessageBox.Show("ボタンをタップしました！！！");
         }
 
         
