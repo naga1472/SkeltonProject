94fc28a04c25c414cb39ec2fd823306ccd8dcdc7
naga1472 <nagata_shinji@skygroup.jp>
2017-11-14 10:29:47 +0900

    変更

--- SkeltonProject/SkeltonProject/Form1.cs
+++ SkeltonProject/SkeltonProject/Form1.cs
@@ -29,7 +29,7 @@ namespace SkeltonProject
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(null);
+            MessageBox.Show("ボタンを押す");
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
