aff48ee568b868b8e9f4208d4dd00fa537b33ee3
naga1472 <nagata_shinji@skygroup.jp>
2017-11-14 10:02:25 +0900

    文言変更

--- SkeltonProject/SkeltonProject/Form1.cs
+++ SkeltonProject/SkeltonProject/Form1.cs
@@ -44,7 +44,7 @@ namespace SkeltonProject
 
         private void Testbutton1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("ボタンをタップしました！！！");
+            MessageBox.Show("ボタンを！！");
         }
 
         
